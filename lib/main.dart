951b305c4c6d49c0885bee4f42b5ee79a0aa0c3b
karla1901 <71739106+karla1901@users.noreply.github.com>
2021-05-11 16:04:09 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ void main() => runApp(MiGaleria());
 class MiGaleria extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(title: "Agenda de fotos", home: new PaginaPrincipal(title: "GridView Galeria de Fotos")); //Fin de material.
+    return new MaterialApp(
+      title: "No tiene efecto", 
+    theme: ThemeData(primarySwatch: Colors.brown),
+      debugShowCheckedModeBanner: false, 
+      home: new PaginaPrincipal(title: "GridView Galeria de Fotos de Karliux")); //Fin de material.
   } //Fin de widget.
 } //Fin de mi agenda fotografica.
 
@@ -40,7 +44,7 @@ class PaginaPrincipalState extends State<PaginaPrincipal> {
 List<Widget> _buildGridTiles(numberOfTiles) {
   List<Container> containers = new List<Container>.generate(numberOfTiles, (int index) {
     //index = 0, 1, 2,...
-    final imageName = index < 9 ? 'images/image0${index + 1}.JPG' : 'images/image${index + 1}.JPG';
+    final imageName = index < 9 ? 'images/image0${index + 1}.jpg' : 'images/image${index + 1}.jpg';
     return new Container(
       child: new Image.asset(imageName, fit: BoxFit.fill),
     );
